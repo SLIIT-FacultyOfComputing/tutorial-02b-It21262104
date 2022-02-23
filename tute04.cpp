2308e2c7a63e111d0ed2115c18ec73805d6270c0
IT21262104 <99266643+It21262104@users.noreply.github.com>
2022-02-23 12:49:57 +0000

    Completer tutorial 02.

--- tute04.cpp
+++ tute04.cpp
@@ -13,17 +13,56 @@ Do not modify the main function.*/
 
 #include <iostream>
 
-long Factorial(int no);
-long nCr(int n, int r);
+long factorial (int no);	//function declaration
+long nCr (int n, int r);
 
-int main() {
+int main ()
+{
   int n, r;
+
   std::cout << "Enter a value for n ";
   std::cin >> n;
+
   std::cout << "Enter a value for r ";
   std::cin >> r;
+
   std::cout << "nCr = ";
-  std::cout << nCr(n,r);
+  
+  std::cout << nCr(n,r);	//function calling
   std::cout << std::endl;
+
   return 0;
+}
+
+long factorial (int no)		//function implemetation
+{
+	long fact = 1;
+	while (no >= 1)
+	{
+		fact = fact * no;
+		no--; 
+	}
+	return fact;
+}
+
+long nCr (int n, int r)
+{
+	long ncr;
+  int rf = 1, nf = 1;
+	
+	n = n - 1;
+	while (n >= 1)
+	{
+		nf = nf * n;
+		n--;
+	}
+	
+	while (r >= 1)
+	{
+		rf = rf * r;
+		r--;
+	}
+	
+	ncr = factorial / (rf * nf);
+	return ncr;
 }
